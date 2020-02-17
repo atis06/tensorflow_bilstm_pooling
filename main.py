96981e7a80fb29593ca6801fb5247816cc7d7196
Attila Kántor <attila.kantor@ericsson.com>
2020-02-17 19:05:03 +0100

    generalized net

---
+++ main.py
@@ -0,0 +1,169 @@
+import csv
+import tensorflow as tf
+import numpy as np
+import nltk
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from nltk.corpus import stopwords
+import gensim
+
+from model import BiRNNWithPooling
+
+print(tf.__version__)
+
+nltk.download('stopwords')
+STOPWORDS = set(stopwords.words('english'))
+
+# Preprocess parameters
+vocab_size = 10000
+embedding_dim = 64
+max_length = 200
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = 'OOV'
+training_portion = .8
+
+# W2V parameters
+w2v_dim = 300
+min_count = 2
+iter = 5
+epochs = 15
+
+
+def preprocess_data():
+    articles = []
+    labels = []
+
+    with open("bbc-text.csv", 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            labels.append(row[0])
+            article = row[1]
+            for word in STOPWORDS:
+                token = ' ' + word + ' '
+                article = article.replace(token, ' ')
+                article = article.replace(' ', ' ')
+            articles.append(article)
+
+    train_size = int(len(articles) * training_portion)
+    train_articles = articles[0: train_size]
+    train_labels = labels[0: train_size]
+
+    validation_articles = articles[train_size:]
+    validation_labels = labels[train_size:]
+
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(train_articles)
+    word_index = tokenizer.word_index
+
+    train_sequences = tokenizer.texts_to_sequences(train_articles)
+    train_padded = pad_sequences(train_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    validation_sequences = tokenizer.texts_to_sequences(validation_articles)
+    validation_padded = pad_sequences(validation_sequences, maxlen=max_length, padding=padding_type,
+                                      truncating=trunc_type)
+
+    label_tokenizer = Tokenizer()
+    label_tokenizer.fit_on_texts(labels)
+
+    reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+    training_label_seq = np.array(label_tokenizer.texts_to_sequences(train_labels))
+    validation_label_seq = np.array(label_tokenizer.texts_to_sequences(validation_labels))
+
+    return train_padded, train_labels, validation_padded, validation_labels, reverse_word_index, articles, training_label_seq, validation_label_seq
+
+
+def decode_article(text, reverse_word_index):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+def train_w2v(articles):
+    splitted_articles = [article.split() for article in articles]
+    model = gensim.models.Word2Vec(iter=iter, size=w2v_dim, workers=4, min_count=min_count)
+    model.build_vocab(splitted_articles)
+    model.train(splitted_articles, total_examples=len(splitted_articles), epochs=epochs)
+
+    return model
+
+
+def apply_w2v(model, train_padded, validation_padded, reverse_word_index):
+    train_articles_w2v = []
+    validation_articles_w2v = []
+
+    for row in train_padded:
+        w2v_sentence = []
+        for word in enumerate(decode_article(row, reverse_word_index).split()):
+            try:
+                w2v_sentence.append(np.asarray(model.wv[word[1]]))
+            except Exception as e:
+                w2v_sentence.append(np.asarray(np.zeros(w2v_dim)))
+        train_articles_w2v.append(np.asarray(w2v_sentence))
+
+    for row in validation_padded:
+        w2v_sentence = []
+        for word in enumerate(decode_article(row, reverse_word_index).split()):
+            try:
+                w2v_sentence.append(np.asarray(model.wv[word[1]]))
+            except Exception as e:
+                w2v_sentence.append(np.asarray(np.zeros(w2v_dim)))
+        validation_articles_w2v.append(np.asarray(w2v_sentence))
+
+    return train_articles_w2v, validation_articles_w2v
+
+
+def next_batch(batch_size, train_articles_w2v, training_label_seq):
+    idx = np.arange(0, len(train_articles_w2v))
+    np.random.shuffle(idx)
+    idx = idx[0:batch_size]
+    data_shuffle = [train_articles_w2v[i] for i in idx]
+    labels_shuffle = [training_label_seq[i] for i in idx]
+
+    return np.asarray(data_shuffle), np.asarray(labels_shuffle)
+
+
+def print_shape(varname, var):
+    with tf.Session() as sess:
+        print('{0} : {1}'.format(varname, tf.shape(var, name=None)))
+
+
+def train_model():
+    train_padded, train_labels, validation_padded, validation_labels, reverse_word_index, articles, training_label_seq, validation_label_seq = preprocess_data()
+    w2v_model = train_w2v(articles)
+    train_articles_w2v, validation_articles_w2v = apply_w2v(w2v_model, train_padded, validation_padded,
+                                                            reverse_word_index)
+
+    model = BiRNNWithPooling()
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(epochs):
+            X_batch, y_batch = next_batch(model.batch_size, train_articles_w2v, training_label_seq)
+
+            X_batch = X_batch.reshape((model.batch_size, model.num_time_steps, model.num_inputs))
+            y_batch = tf.one_hot(y_batch - 1, model.n_classes, axis=-1).eval().reshape(-1, model.n_classes)
+
+            feed_dict = {model.X: X_batch, model.y: y_batch}
+            model.train(sess, feed_dict)
+
+            if epoch % 100 == 0:
+                feed_dict = {model.X: X_batch, model.y: y_batch}
+                mse = model.evaluate(feed_dict)
+                print(epoch, "\tMSE:", mse)
+
+        X_batch_valid = np.asarray(validation_articles_w2v).reshape(
+            (len(validation_articles_w2v), model.num_time_steps, model.num_inputs))
+        y_batch_valid = tf.one_hot(validation_label_seq - 1, model.n_classes, axis=-1).eval().reshape(-1,model.n_classes)
+
+        feed_dict_valid = {model.X: X_batch_valid, model.y: y_batch_valid}
+
+        loss_valid, acc_valid = model.validate(sess, feed_dict_valid)
+        print('---------------------------------------------------------')
+        print("Epoch: {0}, validation loss: {1:.2f}, validation accuracy: {2:.01%}".
+              format(epoch + 1, loss_valid, acc_valid))
+        print('---------------------------------------------------------')
+
+
+train_model()
---
+++ model.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+
+class BiRNNWithPooling:
+
+    def __init__(self):
+        # Just one feature, the time series(embeddig dim)
+        self.num_inputs = 300
+        # Num of steps in each batch (seqlength)
+        self.num_time_steps = 200
+        # Hidden neurons in each rnn layer
+        self.num_hidden = 200
+        # Learning rate you can play with this
+        self.learning_rate = 0.001
+        # How many iterations to go through (training steps)
+        self.epochs = 200
+        # Size of the batch of data
+        self.batch_size = 100
+        # Num of classes
+        self.n_classes = 5
+        # Dropout keep probability
+        self.dropout_keep_prob = 0.8
+        # Pooling (max, avg, None)
+        self.pooling = 'max'
+
+        self.X, self.y = self.__init_placeholders()
+
+        self.W, self.b = self.__init_variables()
+
+        self.optimizer, self.loss, self.output_logits = self.__get_network()
+
+    def __init_placeholders(self):
+        X = tf.placeholder(tf.float32, [None, self.num_time_steps, self.num_inputs])
+        y = tf.placeholder(tf.float32, [None, self.n_classes])
+
+        return X, y
+
+    def __init_variables(self):
+        W = tf.Variable(tf.random_normal(shape=[2 * self.num_hidden, self.n_classes]))
+        b = tf.Variable(tf.zeros([self.n_classes]))
+
+        return W, b
+
+    def __biRNN(self, input):
+        fw_cell = tf.contrib.rnn.BasicLSTMCell(self.num_hidden, forget_bias=1.0)
+        bw_cell = tf.contrib.rnn.BasicLSTMCell(self.num_hidden, forget_bias=1.0)
+
+        if self.dropout_keep_prob is not None:
+                fw_cell=tf.contrib.rnn.DropoutWrapper(fw_cell,output_keep_prob=self.dropout_keep_prob)
+                bw_cell=tf.contrib.rnn.DropoutWrapper(bw_cell,output_keep_prob=self.dropout_keep_prob)
+
+        outputs, _ = tf.nn.bidirectional_dynamic_rnn(fw_cell, bw_cell, input,
+                                                     dtype=tf.float32)
+
+        output_rnn = tf.concat(outputs, axis=2)  # [batch_size,sequence_length,hidden_size*2]
+
+        # this is pooling
+        if self.pooling is not None:
+            if self.pooling == 'max':
+                output_rnn_last = tf.reduce_max(output_rnn,axis=1)  # [batch_size,hidden_size*2]
+            if self.pooling == 'avg':
+                output_rnn_last = tf.reduce_mean(output_rnn, axis=1)
+        else:
+            # this uses the last hidden state as the representation.
+            # [batch_size,hidden_size*2]
+            output_rnn_last = output_rnn[:,-1,:]
+
+        return output_rnn_last
+
+    def __dense_layer(self, input, weights, biases):
+        output_logits = tf.matmul(input, weights) + biases
+        y_pred = tf.nn.softmax(output_logits)
+
+        return output_logits
+
+
+    def __loss(self, output_logits, y):
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output_logits))
+        return loss
+
+    def __accuracy(self, output_logits, y):
+        correct_prediction = tf.equal(tf.argmax(output_logits, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        return accuracy
+
+    def __optimizer(self, loss):
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(loss)
+        return optimizer
+
+    def __get_network(self):
+        rnn_output = self.__biRNN(self.X)
+        output_logits = self.__dense_layer(rnn_output, self.W, self.b)
+        loss = self.__loss(output_logits, self.y)
+        optimizer = self.__optimizer(loss)
+
+        return optimizer, loss, output_logits
+
+    def get_network(self):
+        return self.optimizer, self.loss, self.output_logits
+
+    def train(self, sess, feed_dict):
+        optimizer, loss, output_logits = self.get_network()
+        sess.run(optimizer, feed_dict)
+
+    def evaluate(self, feed_dict):
+        optimizer, loss, output_logits = self.get_network()
+        mse = loss.eval(feed_dict)
+
+        return mse
+
+    def validate(self, sess, feed_dict_valid):
+        optimizer, loss, output_logits = self.get_network()
+        accuracy = self.__accuracy(output_logits, self.y)
+        loss_valid, acc_valid = sess.run([loss, accuracy], feed_dict=feed_dict_valid)
+
+        return loss_valid, acc_valid
